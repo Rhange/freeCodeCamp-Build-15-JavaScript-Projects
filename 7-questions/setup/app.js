b75d7a8d638eae3b02756f9625452484f42b7377
Rhange <rhange0511@gmail.com>
2020-06-17 22:15:59 +0900

    finished 7th project 'question box'
    Make 'question box'
    
    < CSS >
    - using parent, child relation
    - using display: none, inline
    
    < JS >
    - using two method ( traversing the dom and using selectors inside the element )
    - using array.forEach to seperate btns
    - using DOM.classList.toggle

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,2 +1,32 @@
-//using selectors inside the element
-// traversing the dom
+//! using selectors inside the element
+// const questions = document.querySelectorAll(".question");
+
+// const init = () => {
+//   questions.forEach((question) => {
+//     const btn = question.querySelector(".question-btn");
+//     btn.addEventListener("click", () => {
+//       // when switch another question, it will remove prev question 'show-text' class name
+//       questions.forEach((each) => {
+//         question !== each ? each.classList.remove("show-text") : false;
+//       });
+//       question.classList.toggle("show-text");
+//     });
+//   });
+// };
+
+// init();
+
+//! traversing the dom
+const btns = document.querySelectorAll(".question-btn");
+
+const init = () => {
+  btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const title = e.currentTarget.parentElement;
+      const question = title.parentElement;
+      question.classList.toggle("show-text");
+    });
+  });
+};
+
+init();
